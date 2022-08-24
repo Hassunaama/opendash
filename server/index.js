5f5b2a7c11c6dfff500a6a70170590938a1b9afc
Nino Kröger <nino.kroger@gmail.com>
2022-08-24 19:38:23 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ app.get('/', (req, res) => {
     res.send('u are in the wrong place, you should be at opendash.netlify.com or at /api/.')
 })
 
-app.get('/api/', (req, res) => {
+/*app.get('/api/', (req, res) => {
     res.status(404).send('<h1>nope.</h1>')
-})
+})*/
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
