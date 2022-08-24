b8e48f443d8cf3196a8efcd47328168ad76e0b84
Nino Kröger <nino.kroger@gmail.com>
2022-08-24 19:39:39 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 8080
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
@@ -13,12 +13,12 @@ app.get('/', (req, res) => {
     res.send('u are in the wrong place, you should be at opendash.netlify.com or at /api/.')
 })
 
-/*app.get('/api/', (req, res) => {
+app.get('/api/', (req, res) => {
     res.status(404).send('<h1>nope.</h1>')
-})*/
+})
 
-app.listen(port, () => {
+/*app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
     console.log(`http://localhost:${port}`)
     console.log(`http://localhost:${port}/api/`)
-})
+})*/
